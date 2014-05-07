fe85fcac1c73b0579ccc832c47c031391d75505a
Espen Angell Kristiansen <post@espenak.net>
2014-05-08 01:47:29 +0200

    setup.py: Set a better long_description.

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup, find_packages
 execfile('django_dbdev/version.py')
 
 
+long_description = """
+Do you want to make it easy to create and setup isolated
+Postgres or MySQL database servers during development
+of your Django project?
+
+See https://github.com/espenak/django_dbdev
+"""
+
 setup(
     name = 'django_dbdev',
     description = 'Makes it easy to create and manage databases during development.',
@@ -10,7 +18,7 @@ setup(
     version = __version__,
     url = 'http://github.com/espenak/django_dbdev',
     author = 'Espen Angell Kristiansen',
-    long_description='See https://github.com/espenak/django_dbdev',
+    long_description=long_description,
     packages=find_packages(exclude=['dbdev_testproject']),
     install_requires = [
         'Django',
