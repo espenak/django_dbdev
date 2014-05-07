9f8bb335bc1240b4bf4c49bb78dbbe4cfbf782b3
Espen Angell Kristiansen <post@espenak.net>
2014-05-08 00:02:10 +0200

    Removed all the unneeded management commands.

--- django_dbdev/management/commands/dbdev_createdb.py
+++
@@ -1,8 +0,0 @@
-from ._base import BaseDbdevCommand
-
-
-class Command(BaseDbdevCommand):
-    help = 'Creates the database configured in the specified --database config.'
-
-    def dbdev_handle(self):
-        self.dbdev_backend.create_database(self.dbname)
